2fafc03d11dd2756f11579f33428bfeab92cc380
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-11-06 00:29:38 -0600

    Changed devise layouts

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Corresponsal::Application.initialize!
+
+config.to_prepare do
+  Devise::SessionsController.layout "admin"
+  Devise::RegistrationsController.layout "admin" 
+  Devise::ConfirmationsController.layout "admin"
+  Devise::UnlocksController.layout "admin"            
+  Devise::PasswordsController.layout "admin"        
+end
