1821e9e8fde3aa87a120de6b2cf25963ab9d0576
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-11-05 23:06:17 -0600

    Moved logic from view to controller. Added admin landing redirect helper.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,8 +3,13 @@ class AdminController < ApplicationController
   #authorize_resource :class => false
 
 	def index
-    @users = User.all.length
-    @entries = Entry.where("video_id is not null").length
+    if not(current_user.try("roles") and current_user.role? :admin)
+      reset_session
+      redirect_to :signin
+    else
+      @users = User.all.length
+      @entries = Entry.where("video_id is not null").length
+    end
 	end
 
 end
