fb5f0956142a62ce6f15b4698eee9908fb6af7c6
Benjamín Hernández <benjamin.hernandez@in2teck.com>
2013-11-05 22:39:24 -0600

    Added permissions for admin.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,7 @@
 class AdminController < ApplicationController
 
+  authorize_resource :class => false
+
 	def index
     @users = User.all.length
     @entries = Entry.where("video_id is not null").length
